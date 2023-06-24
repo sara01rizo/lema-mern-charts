dad47595a9b330cb303556d1bd6b0d04f03809ea
sara pulido <sara01rizo@gmail.com>
2023-06-25 00:13:33 +0200

    Create: cards to fill with charts, styled with mui material

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -12,10 +12,49 @@ import { Typography, Box, Stack } from '@pankod/refine-mui';
 const home = () => {
   return (
     <Box>
-      <Typography fontSize={25} fontWwight={700} color="#11142D">
-        Dashboard
-      </Typography>
+      <Stack>
+        <Typography fontSize={25} fontWeight={700} color="#11142D">
+          Dashboard
+        </Typography>
 
+        <Box mt="20px"
+          display="flex"
+          flexWrap="wrap"
+          gap={4}
+        >
+          <PieChart
+            title="Properties for sale"
+            value={684}
+            series={[75, 25]}
+            colors={['#475be8', '#e4e8ef']}
+          />
+          <PieChart
+            title="Properties for rent"
+            value={550}
+            series={[75, 25]}
+            colors={['#475ae8', '#e4b8ef']}
+          />
+          <PieChart
+            title="Total customers"
+            value={684}
+            series={[75, 25]}
+            colors={['#478be8', '#e4h8ef']}
+          />
+          <PieChart
+            title="Properties for Cities"
+            value={684}
+            series={[75, 25]}
+            colors={['#485be8', '#e5e8ef']}
+          />
+          Charts
+        </Box>
+
+        <Stack mt="25px" width="100%" direction={{ xs:'column', lg: 'row'}}>
+          <TotalRevenue />
+          <PropertyReferrals />
+        </Stack>
+
+      </Stack>
     </Box>
   )
 }
